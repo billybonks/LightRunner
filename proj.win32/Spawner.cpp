3929cd3cbef65937efbaca2156e936a4db68b144
Sebatien Stettler <sebastienstettler@gmail.com>
2012-09-06 15:39:34 +0200

    Changing computer commit
    as it says no real changes

--- proj.win32/Spawner.cpp
+++ proj.win32/Spawner.cpp
@@ -5,10 +5,10 @@ Spawner::Spawner(Statistics* stats,b2World* world,b2Vec2 initialSpawnLocation){
 	_stats = stats;
 	_world = world;
 	_initialSpawnLocation = initialSpawnLocation;
-	float minDistance = (_stats->GetMaximumVelocity()*32)*5.0f;
-	float maxDistance = (_stats->GetMaximumVelocity()*32)*10.0f;
+	float minDistance = (_stats->GetMaximumVelocity()*32)*10.0f;
+	float maxDistance = (_stats->GetMaximumVelocity()*32)*20.0f;
 	float distance = Random(minDistance,maxDistance);
-	int maxSegments =distance/100;
+	int maxSegments =distance/200;
 	int segments = Random(1,maxSegments);
 	LineSegment segment = GenerateCompoundSegment(distance,segments);
 
@@ -41,18 +41,34 @@ LineSegment Spawner::GenerateCompoundSegment(float distance,int segments){
 			segmentDistance = distanceRemeinder;
 		}else{
 			segmentDistance = Random(minDistancePerSegment,maxDistancePerSegment);
+			//distanceRemeinder = segmentDistance - maxDistancePerSegment;
 		}
 		switch ( structType )
 		{
 		case 1:
-			currentSegment = dynamic_cast<ContinuousLineSegment*>( new StraightLineSegment(_world,_initialSpawnLocation,distance,50,1));
+			currentSegment = dynamic_cast<ContinuousLineSegment*>( new StraightLineSegment(_world,_initialSpawnLocation,segmentDistance,50,1));
 			break;
 		case 2:
-			currentSegment = dynamic_cast<ContinuousLineSegment*>(new InclineLineSegment(_world,_initialSpawnLocation,distance,100,10));
+			currentSegment = dynamic_cast<ContinuousLineSegment*>(new InclineLineSegment(_world,_initialSpawnLocation,segmentDistance,100,10));
 			break;	
 		case 3:
-			currentSegment = dynamic_cast<ContinuousLineSegment*>(new InclineLineSegment(_world,_initialSpawnLocation,distance,100,-10));
-			break;	
+			currentSegment = dynamic_cast<ContinuousLineSegment*>(new InclineLineSegment(_world,_initialSpawnLocation,segmentDistance,100,-10));
+			break;
+		case 4:
+			//stright line gap
+			break;
+		case 5:
+			//Positive Incline Gap
+			break;
+		case 6:
+			//negative Incline Gap
+			break;
+		case 7:
+			//Upward jump tunnel
+			break;
+		case 8:
+			//LongDrop jump tunnel
+			break;
 		}
 		if(i != 1){
 			lastSegment->SetChild(currentSegment,lastSegment);
